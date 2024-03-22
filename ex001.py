73086a7023aa9b5c9200d4c3393e7d70e5df75cf
JappaFast <162744915+JappaFast@users.noreply.github.com>
2024-03-22 15:04:40 -0300

    Add files via upload
    Exercícios Tabuada

---
+++ ex001.py
@@ -0,0 +1,13 @@
+from Tools.scripts.which import msg
+
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+cidade = input('Qual a sua cidade? ')
+
+print('Seu nome é', nome, ',sua idade é,', idade, ',Sua cidade é,', cidade)
+
+msg('Olá mundo!')
+
+print('Olá mundo!')
+msg = 'Olá mundo! '
+print(msg)
